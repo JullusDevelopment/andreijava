51a8b958cafea96ab6bc21f97f59cc445b76032d
Andrei Muresan <muresan.andrei87@gmail.com>
2022-04-11 22:08:51 +0300

    added ex 1

--- src/main/java/w3schools/Exercise1.java
+++ src/main/java/w3schools/Exercise1.java
@@ -3,5 +3,7 @@ package w3schools;
 public class Exercise1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Animal caineMeu = new Animal("Nais");
+        System.out.println(caineMeu.getName());
     }
 }
