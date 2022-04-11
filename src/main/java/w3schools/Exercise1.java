0c42b61365657ea6074ffd634cf583dcdef84e01
Iulian Vlad <iulian.vlad86@gmail.com>
2022-04-11 21:54:23 +0300

    fix text in class

--- src/main/java/w3schools/Exercise1.java
+++ src/main/java/w3schools/Exercise1.java
@@ -2,6 +2,6 @@ package w3schools;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("Hello World");
     }
 }
