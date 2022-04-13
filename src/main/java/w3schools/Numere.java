02176514dfed9ebd1c5664ba6ae22f1ccf7026f6
Andrei Muresan <muresan.andrei87@gmail.com>
2022-04-13 21:37:50 +0300

    primul for folosit

---
+++ src/main/java/w3schools/Numere.java
@@ -0,0 +1,14 @@
+package w3schools;
+
+public class Numere {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,10));
+        int[] myNum = {10, 20, 30, 40};
+        for (int i = 0; i < myNum.length; i++) {
+            System.out.print(myNum[i] +",");
+        }
+
+    }
+
+
+}
